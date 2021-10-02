7440c641ef9c2d9f7fd2e31ead24e0a04a223407
Ankit Jain <72207145+iamaunique@users.noreply.github.com>
2021-10-02 11:39:55 +0530

    Initial App Commit

---
+++ app/src/main/java/com/valdioveliu/valdio/audioplayer/RecyclerView_Adapter.java
@@ -0,0 +1,52 @@
+package com.valdioveliu.valdio.audioplayer;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Valdio Veliu on 16-07-08.
+ */
+public class RecyclerView_Adapter extends RecyclerView.Adapter<ViewHolder> {
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflate the layout, initialize the View Holder
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //Use the provided View Holder on the onCreateViewHolder method to populate the current row on the RecyclerView
+        holder.title.setText(list.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        //returns the number of elements the RecyclerView will display
+        return list.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+}
+
+class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView title;
+    ImageView play_pause;
+}
